d0ed63317ad4abaa8d1e24d6e3e64c277f0029c9
Service Web <sw@gssmail.com>
2024-03-12 17:23:35 -0500

    Commit changes in C:\Websites\Git\emarlow\4854\GCG_4854_CATEGORY.sql

---
+++ GCG_4854_CATEGORY.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_4854_CATEGORY
+ * ========================================== */
+CREATE TABLE "GCG_4854_CATEGORY"(
+ "PARENT_KEY" CHAR(20),
+ "PARENT" CHAR(20),
+ "LEVEL" CHAR(10),
+ "CATEGORY" CHAR(10),
+ "REC_SEQ" CHAR(6));
+
+
